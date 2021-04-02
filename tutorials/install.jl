a069d39f1f31c549c407f46b3d59211086cd28f2
Markus <markus.schmitt@berkeley.edu>
2021-04-02 12:09:20 +0200

    Fixed install.jl

--- tutorials/install.jl
+++ tutorials/install.jl
@@ -28,9 +28,29 @@ function install()
 
     # add all pkgs with specific versions (not pinned)
     @info "Installiere alle Pakete..."
-    pkg"add BenchmarkTools@0.7.0 CSV@0.8.4 CSVFiles@1.0.0 ColorTypes@0.8.1 Cubature@1.5.1 DataFrames@0.22.7 DifferentialEquations@6.16.0
-	Distributions@0.24.15 ExcelFiles@1.0.0 FFTW@1.3.2 Flux@0.8.3 Formatting@0.4.2 HDF5@0.15.4 LaTeXStrings@1.2.1 LsqFit@0.12.0
-	Measurements@2.5.0 Polynomials@2.0.5 ProgressMeter@1.5.0 PyCall@1.92.2 PyPlot@2.9.0 QuantumOptics@0.8.5 Traceur@0.3.1 Zygote@0.6.8"
+    Pkg.add(name="BenchmarkTools", version="0.7.0")
+    Pkg.add(name="CSV", version="0.8.4")
+    Pkg.add(name="CSVFiles", version="1.0.0")
+    Pkg.add(name="ColorTypes", version="0.8.1")
+    Pkg.add(name="Cubature", version="1.5.1")
+    Pkg.add(name="DataFrames", version="0.22.7")
+    Pkg.add(name="DifferentialEquations", version="6.16.0")
+    Pkg.add(name="Distributions", version="0.24.15")
+    Pkg.add(name="ExcelFiles", version="1.0.0")
+    Pkg.add(name="FFTW", version="1.3.2")
+    Pkg.add(name="Flux", version="0.8.3")
+    Pkg.add(name="Formatting", version="0.4.2")
+    Pkg.add(name="HDF5", version="0.15.4")
+    Pkg.add(name="LaTeXStrings", version="1.2.1")
+    Pkg.add(name="LsqFit", version="0.12.0")
+    Pkg.add(name="Measurements", version="2.5.0")
+    Pkg.add(name="Polynomials", version="2.0.5")
+    Pkg.add(name="ProgressMeter", version="1.5.0")
+    Pkg.add(name="PyCall", version="1.92.2")
+    Pkg.add(name="PyPlot", version="2.9.0")
+    Pkg.add(name="QuantumOptics", version="0.8.5")
+    Pkg.add(name="Traceur", version="0.3.1")
+    Pkg.add(name="Zygote", version="0.6.8")
 
     # precompile
     @info "Bereite alle Pakete vor...."
@@ -46,13 +66,33 @@ function install_overwrite()
 
     # install IJulia
     @info "Installiere IJulia"
-    pkg"add IJulia"
+    Pkg.add(name="IJulia")
 
     # add all pkgs with specific versions (not pinned)
     @info "Installiere alle Pakete..."
-    pkg"add BenchmarkTools@0.7.0 CSV@0.8.4 CSVFiles@1.0.0 ColorTypes@0.8.1 Cubature@1.5.1 DataFrames@0.22.7 DifferentialEquations@6.16.0
-	Distributions@0.24.15 ExcelFiles@1.0.0 FFTW@1.3.2 Flux@0.8.3 Formatting@0.4.2 HDF5@0.15.4 LaTeXStrings@1.2.1 LsqFit@0.12.0
-	Measurements@2.5.0 Polynomials@2.0.5 ProgressMeter@1.5.0 PyCall@1.92.2 PyPlot@2.9.0 QuantumOptics@0.8.5 Traceur@0.3.1 Zygote@0.6.8"
+    Pkg.add(name="BenchmarkTools", version="0.7.0")
+    Pkg.add(name="CSV", version="0.8.4")
+    Pkg.add(name="CSVFiles", version="1.0.0")
+    Pkg.add(name="ColorTypes", version="0.8.1")
+    Pkg.add(name="Cubature", version="1.5.1")
+    Pkg.add(name="DataFrames", version="0.22.7")
+    Pkg.add(name="DifferentialEquations", version="6.16.0")
+    Pkg.add(name="Distributions", version="0.24.15")
+    Pkg.add(name="ExcelFiles", version="1.0.0")
+    Pkg.add(name="FFTW", version="1.3.2")
+    Pkg.add(name="Flux", version="0.8.3")
+    Pkg.add(name="Formatting", version="0.4.2")
+    Pkg.add(name="HDF5", version="0.15.4")
+    Pkg.add(name="LaTeXStrings", version="1.2.1")
+    Pkg.add(name="LsqFit", version="0.12.0")
+    Pkg.add(name="Measurements", version="2.5.0")
+    Pkg.add(name="Polynomials", version="2.0.5")
+    Pkg.add(name="ProgressMeter", version="1.5.0")
+    Pkg.add(name="PyCall", version="1.92.2")
+    Pkg.add(name="PyPlot", version="2.9.0")
+    Pkg.add(name="QuantumOptics", version="0.8.5")
+    Pkg.add(name="Traceur", version="0.3.1")
+    Pkg.add(name="Zygote", version="0.6.8")
 
     # precompile
     @info "Bereite alle Pakete vor...."
